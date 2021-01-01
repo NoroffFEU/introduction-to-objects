4fc64ad202d029ae821a1ffc915283fca86bcec8
Connor O'Brien <connor@hifranc.com>
2021-01-01 20:24:52 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,37 @@
+// animal object
+
+// type = "elephant"
+// colour = "grey"
+// number of lgegs = 4
+// domesticated = no
+
+var animal = {
+    type: "elephant",
+    colour: "grey",
+    numberOfLegs: 4,
+    domesticated: false
+};
+
+var type = animal.type;
+
+console.log(type);
+
+
+// example 2
+// vehicle object
+
+// category = "car"
+// colour = "pink"
+// number of wheels = 4
+// number of wings = not applicable
+
+var vehicle = {
+    category: "car",
+    colour: "pink",
+    numberOfWheels: 4,
+    numberOfWings: null
+};
+
+console.log(vehicle);
+
+console.log(vehicle.numberOfWings);
